b89928b5422767da3203bcf81c3822f12cdf695c
Shyam-Has-Your-Anomaly-Mitigated <shyam@shyam.id.au>
2017-12-21 02:08:17 +1100

    Should I mention I'm volunteering for LCA'18? It hasn't happened yet...

--- LaTeX/personal_documentation/shyam.sh
+++ LaTeX/personal_documentation/shyam.sh
@@ -4,4 +4,5 @@ printf \\ec;
 vim shyam.tex;
 ./bash-latex.sh -f=shyam.tex;
 rm shyam.svg  dot.pdf .log;
+./htm.sh
 # EDIT: added `./dot.sh`, and the `rm`s; changed "$1".tex to shyam.tex; renamed from `./latex.sh` to `./shyam.sh`
