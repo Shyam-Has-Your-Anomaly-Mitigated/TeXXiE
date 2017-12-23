ca45b115b88ba36940d8c07b7d9e10c8bf01d27e
Shyam-Has-Your-Anomaly-Mitigated <shyam@shyam.id.au>
2017-12-24 00:58:04 +1100

    every"-thing" should be single command

--- LaTeX/personal_documentation/README.md
+++ LaTeX/personal_documentation/README.md
@@ -1,2 +1,4 @@
+`./shyam.sh`
 1. update `$address` ∧ `$title` ∈ `./htm.sh`
-1. `./shyam.sh`
+1. update DOT
+1. update `/Dailies` ∈ TeX
--- LaTeX/personal_documentation/shyam.sh
+++ LaTeX/personal_documentation/shyam.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 printf \\ec;
-./dot.sh
+vim htm.sh;
+./dot.sh;
 vim shyam.tex;
 ./bash-latex.sh -f=shyam.tex;
 rm shyam.svg  dot.pdf .log;
-./htm.sh
+./htm.sh;
 # EDIT: added `./dot.sh`, and the `rm`s; changed "$1".tex to shyam.tex; renamed from `./latex.sh` to `./shyam.sh`
